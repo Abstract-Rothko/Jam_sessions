892424d8cd3a602cb4e75cb0b66bd72ac8caf53e
Adamu Abdullahi <ysdgas77@gmail.com>
2024-10-15 19:37:21 +0200

    Update csv source

--- 1_mini_project_sessions/4_manga_eda/1_manga_eda.py
+++ 1_mini_project_sessions/4_manga_eda/1_manga_eda.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # Load dataset
-df = pd.read_csv("/storage/3136-3633/Projects/2. Datasets/1. Anime & Manga/best-selling-manga.csv")
+df = pd.read_csv("/.../best-selling-manga.csv")
 
 print(df.shape)   # (187, 8)
 
